ba5d8a8e70d697ac84bf1fa3393362f73ec31d3c
COIRO AGUSTIN FRANCO <95722600+AFCoiro@users.noreply.github.com>
2024-05-25 20:17:28 +0000

    completado travel y search

--- pag/scripts/search.js
+++ pag/scripts/search.js
@@ -1,108 +1,109 @@
-// let containerCardTrip = document.getElementsByClassName("containerCardTrip");
-
-// // Consumiendo la API https://restcountries.com
-// const apiUrl = 'https://restcountries.com/v3.1/all';
-// const paisesSeleccionados = ['Argentina', 'Brasil', 'Ecuador', 'España', 'Francia', 'Italia', 'Irlanda', 'Indonesia', 'Japón', 'Egipto', 'Australia', 'Sudáfrica'];
-
-// async function procesaDatos() {
-//   try {
-//     const datos = await fetch(apiUrl).then(res => res.json());
-//     const paisesFiltrados = datos.filter(pais => paisesSeleccionados.includes(pais.translations.spa.common));
-//     filtroPaises(paisesFiltrados);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// function filtroPaises(paises) {
-//   paises.forEach(pais => {
-//     let nombre = pais.translations.spa.common;
-//     let capital = pais.capital ? pais.capital[0] : 'Capital no disponible';
-
-//     let cardsTrip = document.createElement('div');
-//     cardsTrip.setAttribute('class', 'cardsTrip');
-//     cardsTrip.innerHTML = `
-//       <div>
-//         <img class="imgCardTrip" src="/img/paises/${nombre}.jpg" alt="Imagen de ${nombre}">
-//       </div>
-//       <a class="TextCardTrip" href="travels.html" target="_blank">
-//         <div>
-//           <h3>${pais.flag} ${nombre}</h3>
-//           <h4>Septiembre - 8 Noches</h4>
-//           <p>Vuelo + Hotel ${capital} all inclusive + 3 Excursiones por ${capital}</p>
-//         </div>
-//         <div>
-//           <h4>Precio por persona desde</h4>
-//           <h2>$1.887.237</h2>
-//           <p>Incluye impuestos, tasas y cargos</p>
-//         </div>
-//       </a>
-//     `;
-
-//     cardsTrip.querySelector('.TextCardTrip').addEventListener('click', function () {
-//       const selectedCountryInfo = {
-//         nombre: nombre,
-//         capital: capital,
-//         imagen: `/img/paises/${nombre}.jpg`
-//       };
-//       localStorage.setItem('selectedCountry', JSON.stringify(selectedCountryInfo));
-//     });
-
-//     containerCardTrip[0].append(cardsTrip);
-//   });
-// }
-
-// procesaDatos();
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   // Recuperar la información del país desde localStorage
-//   const storedCountryInfo = JSON.parse(localStorage.getItem('selectedCountry'));
-
-//   if (storedCountryInfo) {
-//     const mainTravel = document.getElementById('mainTravel');
-//     mainTravel.innerHTML = `
-//       <h1>Tour por ${storedCountryInfo.nombre} durante 3 días + vuelo incluido</h1>
-//       <img src="${storedCountryInfo.imagen}" alt="Imagen de ${storedCountryInfo.nombre}">
-//       <p>Capital: ${storedCountryInfo.capital}</p>
-//     `;
-
-//     fetch('../../tours.json')
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data[storedCountryInfo.nombre]) {
-//           const additionalContent = data[storedCountryInfo.nombre].descripcion;
-//           mainTravel.insertAdjacentHTML('beforeend', `
-//             <p>${additionalContent}</p>
-//           `);
-//           console.log("mi contenido"+additionalContent);
-//         }
-//       })
-//       .catch(error => {
-//         console.error("Errorjtjtjtjtjtjtjtj adicional:", error);
-//       });
-
-//     console.log('Información del país recuperada de localStorage:', storedCountryInfo); // Mensaje de consola
-//   } else {
-//     console.error("errorrrrrrrrrrrrrrrrrrrrrrrrr localStorage.");
-//   }
-// });
-
-
 let containerCardTrip = document.getElementsByClassName("containerCardTrip");
+
+//consumiendo api https://restcountries.com/
 const apiUrl = 'https://restcountries.com/v3.1/all';
 const paisesSeleccionados = ['Argentina', 'Brasil', 'Ecuador', 'España', 'Francia', 'Italia', 'Irlanda', 'Indonesia', 'Japón', 'Egipto', 'Australia', 'Sudáfrica'];
+const jsonUrl = '../../tours.json';
 
 async function ProcesarDatos() {
   try {
-    const datos = await fetch(apiUrl);
-    const datosJson = await datos.json();
+    const datosApi = await fetch(apiUrl);
+    const datosA = await datosApi.json();
+    const paisesFiltrados = datosA.filter(pais => paisesSeleccionados.includes(pais.translations.spa.common));
+    const datosJson = await fetch(jsonUrl);
+    const datosJ = await datosJson.json();
 
-     const paisesFiltrados = datosJson.filter(pais => paisesSeleccionados.includes(pais.translations.spa.common));
-
+    fnMostrarPaises(paisesFiltrados, datosJ);
     console.log(paisesFiltrados);
+    console.log(datosJ);
+
   } catch (error) {
-    console.error("*****ERROR*****: "+error);
+    console.error("*****ERROR*****: " + error);
   }
 }
 ProcesarDatos();
+
+function fnMostrarPaises(paisA, paisJ) {
+  paisA.forEach(pais => {
+    let country = pais.translations.spa.common;
+    let capital = pais.capital;
+    let bandera = pais.flag;
+    let mapa = pais.maps.googleMaps;
+    let paisInfo = paisJ[country];
+
+    const cardsTrip = document.createElement("div");
+    cardsTrip.setAttribute('class', 'cardsTrip');
+    cardsTrip.innerHTML = `
+        <div>
+          <img class="imgCardTrip" src="/img/paises/${country}.jpg" alt="Imagen de ${country}">
+        </div>
+        <a class="linkCardTrip" href="travels.html" target="_blank">
+          <div>
+            <h3>${bandera} ${country}</h3>
+            <h4>${paisInfo.duracion}</h4>
+            <p>Vuelo + Hotel ${paisInfo.titulo}</p>
+          </div>
+          <div>
+            <h4>Precio por persona desde</h4>
+            <h2>$${paisInfo.precio}</h2>
+            <p>Incluye impuestos, tasas y cargos</p>
+          </div>
+        </a>
+      `;
+
+
+      cardsTrip.querySelector('.linkCardTrip').addEventListener('click', function () {
+        const infoPaisGuardado = {
+          nombre: country,
+          capital: capital,
+          bandera: bandera,
+          mapa: mapa,
+          imagen: `/img/paises/${country}.jpg`,
+          descripcion: paisInfo.descripcion,
+          precio: paisInfo.precio,
+          titulo: paisInfo.titulo,
+          salida: paisInfo.salida,
+          duracion: paisInfo.duracion,
+        };
+        localStorage.setItem('infoPais', JSON.stringify(infoPaisGuardado));
+      });
+  
+      containerCardTrip[0].append(cardsTrip);
+    });
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const infoPaisesGuardados = JSON.parse(localStorage.getItem('infoPais'));
+
+  if (infoPaisesGuardados) {
+    const mainTravel = document.getElementById('mainTravel');
+    mainTravel.innerHTML = `
+    <div class="contTravel">
+    <section class="travelInfo">
+        <img src="${infoPaisesGuardados.imagen}" alt="${infoPaisesGuardados.nombre}">
+        <div class="contTravelText containerWidth">
+            <h2>Descripcion</h2>
+            <h3>${infoPaisesGuardados.bandera}  ${infoPaisesGuardados.titulo}</h3>
+            <h4>Plan Cuota Simple - 12 cuotas fijas</h4>
+            <ul>
+                <li>Duración: <span>${infoPaisesGuardados.duracion}</span></li>
+                <li>Destinos: <span>${infoPaisesGuardados.capital}</span></li>
+                <li>Saliendo desde: <span>${infoPaisesGuardados.capital}</span></li>
+                <li>Fecha de salida: <span>${infoPaisesGuardados.salida}</span></li>
+            </ul>
+            <h3>Sobre ${infoPaisesGuardados.nombre}</h3>
+            <p>${infoPaisesGuardados.descripcion}</p>
+        </div>
+    </section>
+    <section class="travelPay">
+        <h2>${infoPaisesGuardados.bandera} ${infoPaisesGuardados.titulo}</h2>
+        <h3>📅Salida: ${infoPaisesGuardados.salida}</h3>
+        <h3>👤👤2 pasajeros,🛏️ 1 habitación</h3>
+        <h3>Precio total - 2 pasajeros: <span>$${infoPaisesGuardados.precio}</span></h3>
+        <a href="contact.html" class="botonContacto">Consultar</a>
+        
+    </section>
+</div>
+    `;
+  }
+});
